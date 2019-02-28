8e2646880cc90db3d535a9049dca6897f7af61f1
trevorcapozza <37559057+trevorcapozza@users.noreply.github.com>
2019-02-27 17:47:04 -0800

    Update main.js

--- localstorage/main.js
+++ localstorage/main.js
@@ -1,20 +1,11 @@
 /* eslint-env browser, jquery */
 
-let darkThemeEnabled;
-
-$(document).ready(() => {
-	const store = localStorage.getItem("dark-theme-enabled");
-	const bool = JSON.parse(store); // convert "false"/"true" to false/true
-	if (bool) {
-		darkThemeEnabled = true;
-		$("section").toggleClass("dark");
-		$("ul").toggleClass("active");
-	}
-
-	$("ul").click(() => {
-		$("ul").toggleClass("active");
-		$("section").toggleClass("dark");
-		darkThemeEnabled = !darkThemeEnabled;
-		localStorage.setItem("dark-theme-enabled", darkThemeEnabled);
-	});
+$(document).ready(function() {
+  var darkMode = localStorage.getItem('dark') || false;
+  $('ul').click(function() {
+    $('ul').toggleClass('active')
+    $('section').toggleClass('dark');
+    darkMode = !darkMode;
+    localStorage.setItem("dark", darkMode);
+  });
 });
